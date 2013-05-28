256078b3800f6aaba782930937e7182258057820
Frank Caron <frank@frankcaron.com>
2013-05-27 21:15:46 -0400

    New Notepad Collection
    Added the list class, "Notepad", for the collection of notes and new
    tests.

--- venv/src/Notes.py
+++ venv/src/Notes.py
@@ -1,7 +1,7 @@
 #
 # Class: Note
 # 
-# This is the base note class for a Note object which will contain the content.
+# The base "Note" object which will contain and persist the content.
 # 
 # 2013, Frank Caron
 
@@ -16,13 +16,13 @@ class Note:
     #Constructor method
     def __init__(self):
         #This will eventually sync with the ID in the DB
-        self.id = 0
-    
+        self._note_id = 0
+            
     #Function to set Note
     def create_note(self, content_to_add):
         self._note_creator = 'system_default'
         self._note_content = content_to_add
-        self._note_title = self.determine_title(content_to_add)
+        self._note_title = self.determine_title(content_to_add + '\n')  #include a '\n' in case it's a one-liner
     
     #Updates the note with the specified content
     def update_note(self, content_to_update):
@@ -33,3 +33,31 @@ class Note:
     #by finding the first new line character.
     def determine_title(self, title_to_cut):
         return title_to_cut.split('\n', 1)[1]
+
+#
+# Class: NoteList
+# 
+# The List Collection for the notes resources
+# 
+# 2013, Frank Caron
+
+class Notepad:
+    
+    #Private Variables
+    _id = 0
+    _notes = []
+    
+    #Constructor method
+    def __init__(self):
+    #This will eventually sync with the ID in the DB
+        self._id = 0
+
+    #Add note to the list
+    def add_note(self, content):
+        new_note = Note()
+        new_note.create_note(content)
+        self._notes.append(new_note)
+        
+    #Remove note at id
+    def remove_note(self, note_id):
+        del self._notes[note_id]
--- venv/src/test.py
+++ venv/src/test.py
@@ -5,10 +5,20 @@
 #
 
 #Import Note Class
-from Note import Note
+from Notes import Note
+from Notes import Notepad
+
+#
+# ===========
+# Tier 1 Test: No Lists
+# ===========
+#
+
+print 'test 1: '
 
 #Make a new note
 temp = Note()
+temp.create_note('test')
 
 #Set content
 temp.update_note('tester\nloles')
@@ -16,3 +26,30 @@ temp.update_note('tester\nloles')
 #Print the note contents
 print 'my content is ' + temp._note_content
 print 'my title is ' + temp._note_title
+
+#
+# ===========
+# Tier 2 Test: Lists
+# ===========
+#
+
+print '\ntest 2: '
+
+#Make a New List
+temp2 = Notepad()
+
+#Add some notes
+temp2.add_note('note1')
+temp2.add_note('note2')
+temp2.add_note('note3')
+
+#Print the deets
+print 'There are ' + str(len(temp2._notes)) + ' notes'
+print 'The second note has the following content: ' + temp2._notes[1]._note_content
+
+#Remove the second note
+temp2.remove_note(1)
+
+#Print the deets
+print 'There are ' + str(len(temp2._notes)) + ' notes'
+print 'The second note has the following content: ' + temp2._notes[1]._note_content
